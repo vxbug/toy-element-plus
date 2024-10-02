33605d00744a32c5b7e6f257e3769d0916a800af
vxbug <1367758108@qq.com>
2024-10-02 18:48:05 +0800

    :tada: first commit

---
+++ packages/components/Button/Button.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps({
+    name: "MdButton"
+
+})
+
+</stript>
+
+<template>
+    <button style='background-color:bule; color:red'> this is a button </button>
+ </template>
+
+<style></style>
---
+++ packages/core/components.ts
@@ -0,0 +1,6 @@
+// components.ts
+
+import { MdButton } from "@toy-element/components";
+import type { Plugin } from "vue";
+
+export default [MdButton] as Plugin[];
